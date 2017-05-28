78a7c6cfc6872acffa00ef05d76a7295f44cf78f
Çağdaş Kurultay Kalkan <cagdaskurultaykalkan@gmail.com>
2017-05-28 22:56:22 +0300

    Draw Chart, Place Characters

--- main.py
+++ main.py
@@ -6,12 +6,24 @@ class Main:
     max_height = 5
     character_alive = True
     character_won = False
-    monster_awake = False
+    monster_awake = True # Should be false in production
     monster_awakened = False
     monster_move_per_turn = 2
 
     def __init__(self):
         self.display_menu()
+
+    def place_character(self):
+        self.character_position = [0, 0]
+
+    def place_monster(self):
+        self.monster_position = [1, 1]
+
+    def place_trap(self):
+        self.trap_position = [0, 1]
+
+    def place_flash(self):
+        self.flask_position = [1, 0]
 
     def display_menu(self):
         menu_list = ['Start New Game', '[Saved Game]', '[Load Game]', 'Customize Setup', 'Exit']
@@ -42,5 +54,34 @@ class Main:
             print('That wasn\'nt a valid option. Try again.')
             self.display_menu()
 
+    def draw_grid(self):
+        height = self.max_height
+        width = self.max_width
+
+        for y in range(0, height):
+            for x in range(0, width):
+                y = str(y)
+                x = str(x)
+                char_x = str(self.character_position[0])
+                char_y = str(self.character_position[1])
+                if str(self.monster_position[0]) == x and str(
+                        self.monster_position[1]) == y and self.monster_awake == True:
+                    sys.stdout.write('M')
+                elif char_x == x and char_y == y:
+                    sys.stdout.write('X')
+                elif str(self.trap_position[0]) == x and str(self.trap_position[1]) == y:
+                    sys.stdout.write('T')
+                elif str(self.flask_position[0]) == x and str(self.flask_position[1]) == y:
+                    sys.stdout.write('F')
+                else:
+                    sys.stdout.write('?')
+
+            sys.stdout.write('\r\n')
+
 
 monster = Main()
+monster.place_character()
+monster.place_monster()
+monster.place_trap()
+monster.place_flash()
+monster.draw_grid()
